aeeb8baba6a5904566c45e9f0b173f6975240c5d
Jiulin Zhu <jl.zhu@cloudcircus.co.jp>
2022-10-28 13:26:55 +0900

    first hosting publish

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
-
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
 Amplify.configure(awsExports);
+
+createApp(App).mount('#app')
